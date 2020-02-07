8045ee1a8fdaa635c4b64508f2f6d4b00a61388c
Danielle <dnix@sfu.ca>
2020-02-06 19:48:21 -0800

    Maze revealer completed.

--- src/model/MazeRevealer.java
+++ src/model/MazeRevealer.java
@@ -1,4 +1,13 @@
 package model;
 
 public class MazeRevealer {
+    private final int WALLSHOWN = 4;
+    private final int SPACESHOWN = 5;
+
+    public void reveal(int[][] maze, String mouseCoord) {
+    }
+
+    public void revealFullMaze(int[][] maze) {
+
+    }
 }
