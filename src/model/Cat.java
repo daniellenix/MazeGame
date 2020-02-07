6caec5fad37834c5e120602de09476b39e0c51d4
Riya Dhariwal <rdhariwa@sfu.ca>
2020-02-06 19:47:42 -0800

    Inputted file structure for GamePlay, Cat, Mouse, and InputTokens.

--- src/model/Cat.java
+++ src/model/Cat.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Cat {
+    private ArrayList<String> emptySpaces = new ArrayList<>();
+
+    public boolean isEmptySpace(int[][] maze, String catCoord){
+        return true;
+    }
+
+    public void inputEmptySpaces(int[][] maze) {
+
+    }
+
+    public String pickRandomSpace (ArrayList<String> emptySpaces) {
+        return null;
+    }
 }
--- src/model/GamePlay.java
+++ src/model/GamePlay.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 public class GamePlay {
+    public boolean didMouseGetCheese(String cheesePosition, String mousePosition){
+        return true;
+    }
+
+    public boolean didCatGetMouse(ArrayList<String> catPositions, String mousePosition){
+        return true;
+    }
+
+    GamePlay(){
+
+    }
 }
