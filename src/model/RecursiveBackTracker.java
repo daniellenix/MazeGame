7d54aaec2c6b11b30baafe379d79af8b314821ca
Riya Dhariwal <rdhariwa@sfu.ca>
2020-02-06 20:59:14 -0800

    Updated RecursiveBackTracker to start making the maze.

--- src/model/RecursiveBackTracker.java
+++ src/model/RecursiveBackTracker.java
@@ -8,25 +8,35 @@ public class RecursiveBackTracker {
     private final int WALL_NOT_SHOWN = 3;
     private final int WALL_SHOWN = 4;
     private final int SPACE_NOT_SHOWN = 6;
-    private int[][] maze = new int[20][15];
+    private static int[][] maze = new int[20][15];
     private Stack<Cell> exploredSpaces = new Stack<>();
     private Cell initialCell = new Cell(0,0);
     private ArrayList<Cell> neighbours = new ArrayList<>();
+
+    public static void main(String[] args) {
+        RecursiveBackTracker m = new RecursiveBackTracker();
+        m.setOutsideWalls();
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 15; j++) {
+                System.out.print(maze[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
     public void makeMaze(){
         exploredSpaces.push(initialCell);
 
     }
 
-    public void setOutsideWalls(int[][] maze) {
-        for (int i = 0; i < maze.length; i++) {
+    public void setOutsideWalls() {
+        for (int i = 0; i < 15; i++) {
             maze[0][i] = WALL_SHOWN;
-            maze[20][i] = WALL_SHOWN;
+            maze[19][i] = WALL_SHOWN;
         }
-
-        for (int i = 0; i < maze.length; i++) {
+        for (int i = 0; i < 20; i++) {
             maze[i][0] = WALL_SHOWN;
-            maze[i][15] = WALL_SHOWN;
+            maze[i][14] = WALL_SHOWN;
         }
     }
 
