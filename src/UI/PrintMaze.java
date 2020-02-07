b589d32307aa66ab1927fe6ba1d83597a618eafb
Riya Dhariwal <rdhariwa@sfu.ca>
2020-02-06 19:50:59 -0800

    Inputted file structure for PrintMaze.

--- src/UI/PrintMaze.java
+++ src/UI/PrintMaze.java
@@ -1,4 +1,24 @@
-package UI;
+package ui;
+
+import java.util.Scanner;
 
 public class PrintMaze {
+    private Scanner userInput;
+
+    public void displayMaze(){
+
+    }
+
+    public void userDirection(){
+
+    }
+
+    public void displayMenu(){
+
+    }
+
+    public void displayApplication(){
+
+    }
+
 }
